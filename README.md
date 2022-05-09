de97186ac257a95d04323de31ed050325fc3d310
svbutko <svbutko@hotmail.com>
2022-05-09 11:23:36 +0300

    Add initial config and files

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # eslint-config-node-strong
-ESLint strong config for NodeJS development
+
+[![npm-version](https://img.shields.io/npm/v/eslint-config-node-strong)](https://www.npmjs.com/package/eslint-config-node-strong)
+[![npm-downloads](https://img.shields.io/npm/dt/eslint-config-node-strong)](https://www.npmjs.com/package/eslint-config-node-strong)
+
+[![GitHub Followers](https://img.shields.io/github/followers/svbutko?label=Follow%20%40svbutko&style=social)](https://github.com/svbutko)
+[![Twitter Follow](https://img.shields.io/twitter/follow/svbutko?label=Follow%20%40svbutko&style=social)](https://twitter.com/svbutko)
+
+<a href="https://www.buymeacoffee.com/svbutko"><img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=svbutko&button_colour=FFDD00&font_colour=000000&font_family=Cookie&outline_colour=000000&coffee_colour=ffffff"></a>
+
+ESLint strong (strict) config for NodeJS development
+
+## Quick start
+
+### Installation
+
+#### yarn
+
+```sh
+yarn add -D eslint eslint-config-node-strong @typescript-eslint/eslint-plugin eslint-plugin-import eslint-plugin-jest
+```
+
+#### npm
+
+```sh
+npm install --save-dev eslint eslint-config-node-strong @typescript-eslint/eslint-plugin eslint-plugin-import eslint-plugin-jest
+```
+
+### Usage
+
+Add `"node-strong"` to the "extends" array in your `.eslintrc.*` file. Make sure to put it **last,** so it gets the chance to override other configs.
+
+```json
+{
+  "extends": [
+    "...",
+    "node-strong"
+  ]
+}
+```
+
+## Plugins included in this project
+
+- [@typescript-eslint/eslint-plugin]
+- [eslint-plugin-import]
+- [eslint-plugin-jest]
+
+[@typescript-eslint/eslint-plugin]: https://github.com/typescript-eslint/typescript-eslint
+[eslint-plugin-import]: https://github.com/benmosher/eslint-plugin-import
+[eslint-plugin-jest]: https://github.com/jest-community/eslint-plugin-jest
---
+++ index.js
@@ -0,0 +1,135 @@
+module.exports = {
+    plugins: [
+        "@typescript-eslint",
+        "import",
+        "jest",
+    ],
+    rules: {
+        //jest
+        "jest/no-disabled-tests": "error",
+        "jest/no-focused-tests": "error",
+        "jest/no-identical-title": "error",
+        "jest/prefer-to-have-length": "warn",
+        "jest/valid-expect": "error",
+        "jest/no-commented-out-tests": "error",
+        "jest/expect-expect": "error",
+        "jest/no-conditional-expect": "error",
+        "jest/no-export": "error",
+        "jest/no-jest-import": "warn",
+        "jest/no-mocks-import": "error",
+        "jest/no-standalone-expect": "error",
+        "jest/no-test-return-statement": "error",
+        "jest/valid-describe-callback": "error",
+        "jest/valid-title": "error",
+
+        //import
+        "import/no-default-export": "error",
+        "import/no-unassigned-import": "error",
+
+        //typescript-eslint
+        "@typescript-eslint/strict-boolean-expressions": "off",
+        "max-classes-per-file": "off",
+        "@typescript-eslint/unbound-method": "off",
+        "@typescript-eslint/no-shadow": ["error"],
+
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/array-type": "error",
+        "@typescript-eslint/await-thenable": "error",
+        "@typescript-eslint/consistent-type-assertions": "error",
+        "@typescript-eslint/consistent-type-definitions": "error",
+        "@typescript-eslint/indent": "off",
+        "@typescript-eslint/member-delimiter-style": "error",
+        "@typescript-eslint/no-empty-function": "error",
+        "@typescript-eslint/no-for-in-array": "error",
+        "@typescript-eslint/no-misused-new": "error",
+        "@typescript-eslint/no-non-null-assertion": "off",
+        "@typescript-eslint/no-unnecessary-qualifier": "error",
+        "@typescript-eslint/no-var-requires": "error",
+        "@typescript-eslint/prefer-for-of": "error",
+        "@typescript-eslint/prefer-namespace-keyword": "error",
+        "@typescript-eslint/promise-function-async": "error",
+        "@typescript-eslint/restrict-plus-operands": "off",
+        "@typescript-eslint/semi": [
+            "error",
+            "always"
+        ],
+        "@typescript-eslint/triple-slash-reference": "error",
+        "@typescript-eslint/type-annotation-spacing": "error",
+        "@typescript-eslint/unified-signatures": "error",
+        "camelcase": "error",
+        "complexity": "error",
+        "constructor-super": "error",
+        "curly": "error",
+        "default-case": "error",
+        "guard-for-in": "error",
+        "id-blacklist": [
+            "error",
+            "any",
+            "Number",
+            "number",
+            "String",
+            "string",
+            "Boolean",
+            "boolean",
+            "Undefined",
+            "unknown",
+            "Array",
+            "void",
+            "never",
+            "object",
+            "Object",
+            "Symbol",
+            "await",
+            "async",
+            "bigint",
+            "BigInt",
+        ],
+        "id-match": "error",
+        "max-len": [
+            "error",
+            {
+                "code": 140
+            }
+        ],
+        "new-parens": "error",
+        "no-bitwise": "error",
+        "no-caller": "error",
+        "no-cond-assign": "error",
+        "no-empty": "error",
+        "no-eval": "error",
+        "no-fallthrough": "error",
+        "no-invalid-this": "off",
+        "no-multiple-empty-lines": "error",
+        "no-new-wrappers": "error",
+        "no-redeclare": "error",
+        "no-shadow": "off",
+        "no-trailing-spaces": "error",
+        "no-undef-init": "error",
+        "no-underscore-dangle": "off",
+        "no-unsafe-finally": "error",
+        "no-unused-expressions": [
+            "error",
+            {
+                "allowShortCircuit": true
+            }
+        ],
+        "no-unused-labels": "error",
+        "no-var": "error",
+        "no-void": "error",
+        "one-var": [
+            "error",
+            "never"
+        ],
+        "padding-line-between-statements": [
+            "error",
+            {
+                "blankLine": "always",
+                "prev": "*",
+                "next": "return"
+            }
+        ],
+        "prefer-const": "error",
+        "use-isnan": "error",
+        "valid-typeof": "error",
+    }
+};
